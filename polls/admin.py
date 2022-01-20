97d11b4cedcad3c4ed8187f94c68ab11813c94ba
Yuri Guedes <yuri@webemlata.com.br>
2022-01-20 14:09:48 -0300

    Melhorando a forma de inserir questions e choieces

--- polls/admin.py
+++ polls/admin.py
@@ -1,18 +1,19 @@
 from django.contrib import admin
-
-# Register your models here.
-
 from .models import Choice, Question
 
 class ChoiceInline(admin.StackedInline):
     model = Choice
-    extra = 3
-    
+    extra = 1
+
 class QuestionAdmin(admin.ModelAdmin):
     # fields = ['pub_date', 'question_text']
     fieldsets = [
         (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date']}),
+    #     ('Date information', {'fields': ['pub_date']}),
+    # ]
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    inlines = [ChoiceInline]
+
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
+# admin.site.register(Choice)
